a553c7c61c678acec65c663138643c046e9ef67f
Higor <44784223+higoraugustorj@users.noreply.github.com>
2023-12-07 16:58:08 -0300

    feat: POST implementado

--- src/main/java/com/higor/restapi/controller/ClienteController.java
+++ src/main/java/com/higor/restapi/controller/ClienteController.java
@@ -3,9 +3,8 @@ package com.higor.restapi.controller;
 import com.higor.restapi.model.Cliente;
 import com.higor.restapi.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,5 +16,11 @@ public class ClienteController {
     @GetMapping
     public List<Cliente> listar() {
         return clienteRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public List<Cliente> adicionar(@RequestBody List<Cliente> novoCliente) {
+        return clienteRepository.saveAll(novoCliente);
     }
 }
